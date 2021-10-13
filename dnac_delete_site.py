bde37dbf997017bd2d9b813eb7aa13b4e2e0b6af
Rick Bauer <rickbaue@cisco.com>
2021-10-13 13:49:51 -0400

    Update dnac_delete_site.py

--- dnac_delete_site.py
+++ dnac_delete_site.py
@@ -1,12 +1,11 @@
 from dnacentersdk import DNACenterAPI
 from pprint import pprint
-from dnacreds import DNAC_USER, DNAC_PASS
-import json
+from dnacreds import DNAC_USER, DNAC_PASS, DNAC_URL, DNAC_VER
 import time
 
 
 #  create_site(site=None, type=None, headers=None, payload=None, active_validation=True, **request_parameters)
-api = DNACenterAPI(username= DNAC_USER, password= DNAC_PASS, base_url='https://10.87.125.20', version='2.2.2.3', verify=False)
+api = DNACenterAPI(username= DNAC_USER, password= DNAC_PASS, base_url=DNAC_URL, version=DNAC_VER, verify=False)
 
 delete_floor = api.sites.delete_site(site_id='9a4eef54-5bca-48d8-9966-4c9d488f8f4d')
 time.sleep(3)
