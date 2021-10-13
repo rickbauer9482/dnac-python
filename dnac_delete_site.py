6b9beba2602d8a8ea25f45d87260c4c9ed36ca7b
Rick Bauer <rickbaue@cisco.com>
2021-10-13 13:54:24 -0400

    Update dnac_delete_site.py

--- dnac_delete_site.py
+++ dnac_delete_site.py
@@ -7,8 +7,8 @@ import time
 #  create_site(site=None, type=None, headers=None, payload=None, active_validation=True, **request_parameters)
 api = DNACenterAPI(username= DNAC_USER, password= DNAC_PASS, base_url=DNAC_URL, version=DNAC_VER, verify=False)
 
-delete_floor = api.sites.delete_site(site_id='9a4eef54-5bca-48d8-9966-4c9d488f8f4d')
+delete_floor = api.sites.delete_site(site_id='FLOOR ID FROM GET SITES')
 time.sleep(3)
-delete_building = api.sites.delete_site(site_id='9f7844d2-2c93-4c77-b268-ae08f56ef18e')
+delete_building = api.sites.delete_site(site_id='BUILDING ID FROM GET SITES')
 time.sleep(3)
-delete_area = api.sites.delete_site(site_id='1ee1b8cb-7b62-47c9-ba47-52e6e3cc78c1')
+delete_area = api.sites.delete_site(site_id='AREA ID FROM GET SITES')
