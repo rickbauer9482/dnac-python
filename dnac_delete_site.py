162ebfd4431bc27ad6b655e1f5e8d90b34a6d854
Rick Bauer <rickbaue@cisco.com>
2021-10-13 13:56:02 -0400

    Update dnac_delete_site.py

--- dnac_delete_site.py
+++ dnac_delete_site.py
@@ -7,6 +7,8 @@ import time
 #  create_site(site=None, type=None, headers=None, payload=None, active_validation=True, **request_parameters)
 api = DNACenterAPI(username= DNAC_USER, password= DNAC_PASS, base_url=DNAC_URL, version=DNAC_VER, verify=False)
 
+# Parent containers cannot be deleted when children are present so must delete in reverse order from create
+# Use dnac_get_sites.py to find the ID of the site object you wish to delete
 delete_floor = api.sites.delete_site(site_id='FLOOR ID FROM GET SITES')
 time.sleep(3)
 delete_building = api.sites.delete_site(site_id='BUILDING ID FROM GET SITES')
