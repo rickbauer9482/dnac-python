8542db8e303a7fe2ecc0094069fc1aec56c9ef2b
Rick Bauer <rickbaue@cisco.com>
2023-09-12 15:35:16 -0400

    Update dnac-get-devices.py

--- dnac-get-devices.py
+++ dnac-get-devices.py
@@ -7,15 +7,17 @@
 # Or you can import creds from a file or vault
 # from dnacreds import DNAC_USER, DNAC_PASS, DNAC_URL, DNAC_VER
 
+from dnacentersdk import DNACenterAPI
+from pprint import pprint
 import os
+
 DNAC_USER = os.environ.get("DNA_CENTER_USERNAME")
 DNAC_PASS = os.environ.get("DNA_CENTER_PASSWORD")
 DNAC_URL = os.environ.get("DNA_CENTER_BASE_URL")
 DNAC_VER = os.environ.get("DNA_CENTER_VERSION")
-from dnacentersdk import DNACenterAPI
-from pprint import pprint
-
 
 api = DNACenterAPI(username=DNAC_USER, password=DNAC_PASS, base_url=DNAC_URL, version=DNAC_VER, verify=False)
 
-mydevice = api.devices.get_device_list(serialNumber='FJC25051UUC')
+mydevice = api.devices.get_device_list(serialNumber='YOU_DEVICES_SN#')
+
+pprint(mydevice)
